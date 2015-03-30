7a1d90b74afea91392728c20297f490d76d7718a
Davy Simon <etudiant@w7-b317-11-sd>
2015-03-30 12:50:11 +0200

    initialisation de l'arraylist client et du constructeur de la classe Banque

--- src/tp7/Banque.java
+++ src/tp7/Banque.java
@@ -1,5 +1,12 @@
 package tp7;
 
+import java.util.ArrayList;
+
 public class Banque {
+	private ArrayList<Client> lesClients;
 	
+	public Banque() {
+		lesClients = new ArrayList<Client>();
+	}
+
 }
